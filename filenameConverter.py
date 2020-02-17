1cf9815b5eb68d269788660f11d79e7e2c942498
Sayan Samanta <reach2sayan@gmail.com>
2020-02-17 16:28:38 -0500

    added a fix to the initial preprocessed filenames

---
+++ filenameConverter.py
@@ -0,0 +1,15 @@
+import numpy as np
+from pathlib import Path
+import os
+import sys
+from tqdm import tqdm
+
+datadir_filenames = Path('./images/processed_images/')
+
+for (directory, _ , image_names) in os.walk(datadir_filenames):
+        for image_name in tqdm(image_names):
+            temp = [int(i) for i in list(image_name) if i.isdigit()]
+            image_num = ''.join([str(num) for num in temp])
+            image_path = os.path.join(directory, image_name)
+            new_name = os.path.join(directory, 'Train_'+image_num+'_f.png')
+            os.rename(image_path,new_name)
